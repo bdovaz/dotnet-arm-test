403a4d7a86437547d6e5a1911aa1cdb0ba5a3761
Borja Domínguez <borja.dominguez@hotmail.com>
2022-12-09 10:13:33 +0100

    Test

--- test/Program.cs
+++ test/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using test;
+
+IWebHost host = WebHost.CreateDefaultBuilder(args)
+    .ConfigureServices((hostContext, services) =>
+    {
+        services.AddMemoryCache();
+
+        services.AddLogging(loggingBuilder =>
+        {
+            loggingBuilder.AddConfiguration(hostContext.Configuration.GetSection("Logging"));
+        });
+    })
+    .UseStartup<Startup>()
+    .Build();
+
+host.Run();
